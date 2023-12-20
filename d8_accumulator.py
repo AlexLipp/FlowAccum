e19dc53f7eadfeaa7cbfe4c2bfd673fc30dbdee2
Alex Lipp <alex@lipp.org.uk>
2023-12-20 15:25:17 +0000

    add numpy import

--- d8_accumulator.py
+++ d8_accumulator.py
@@ -7,6 +7,26 @@ modified from Braun & Willet (2013) DOI: 10.1016/j.geomorph.2012.10.008. This is
 original Landlab implementation as we use an iterative build_stack algorithm (much faster). Most of the code is written 
 in Cython for speed. The approach is linear w.r.t. the number of nodes in the network. Class is designed to be used with 
 geospatial rasters, but can also be used with a numpy array of D8 flow directions. 
+
+## Installation 
+
+To install run: 
+python setup.py build_ext --inplace
+
+## Example of use 
+
+>>> from d8_accumulator import D8Accumulator, write_geotiff
+>>> import numpy as np 
+>>> accumuator = D8Accumulator("d8.tif")
+>>> # Create an array of cell areas
+>>> cell_area = np.ones(len(accumulator.receivers)) * 100 # 100 m^2 cell area
+>>> # Calculate drainage area in m^2
+>>> drainage_area = accumulator.accumulate(weights=cell_area)
+>>> # Calculate number of upstream nodes
+>>> number_nodes = accumulator.accumulate()
+>>> # Write the results to a geotiff
+>>> write_geotiff("drainage_area.tif", drainage_area, accumulator.ds)
+
 """
 
 import warnings
